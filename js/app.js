917d6320171f5154991da20d8045ee062c9de829
Nadia Ahmed <nadiaalshezawy@gmail.com>
2018-10-15 11:21:04 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var ViewModel = function(){
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.level = ko.observable('newborn');
+
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.imgAttribtution =ko.observable('https://www.flickr.com/photos/big');
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() +1);
+    if(this.clickCount()<10)
+     { this.level('newborn');}
+    else if (this.clickCount()<110 && this.clickCount()>10) {
+      this.level('infant');
+    }
+    else if (this.clickCount()<200 && this.clickCount()>=110)
+     {this.level('teen');}
+  };
+}
+
+ko.applyBindings(new ViewModel());
