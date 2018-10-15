7ea52518c32c142d667db57d98fede22dceff873
Nadia Ahmed <nadiaalshezawy@gmail.com>
2018-10-15 12:45:32 +0400

    nick name binding

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var ViewModel = function(){
 
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribtution =ko.observable('https://www.flickr.com/photos/big');
+  this.nickname=ko.observableArray(['Tabtab','t-bone','Mr.t','tabita']);
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() +1);
     if(this.clickCount()<10)
